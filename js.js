c177a83ebf224e29caa193bd16153d384bc7bf3b
sirivus <louishsu13@gmail.com>
2022-02-10 15:11:12 -0800

    fgh

---
+++ js.js
@@ -0,0 +1,55 @@
+height.onClick
+
+var mainframe = document.getElementById('mainframe')
+    requestFullscreen(this)
+
+    const heightOutput = document.querySelector('#height');
+    const widthOutput = document.querySelector('#width');
+    
+    function reportWindowSize() {
+      heightOutput.textContent = window.innerHeight;
+      widthOutput.textContent = window.innerWidth;
+    }
+    
+    window.onresize = reportWindowSize;
+    
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.9, 1000 );
+
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
+
+    const geometry = new THREE.BoxGeometry();
+    const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+    const cube = new THREE.Mesh( geometry, material );
+    scene.add( cube );
+
+    camera.position.z = 5;
+
+    function animate() {
+        requestAnimationFrame( animate );
+
+        cube.rotation.x += 0.01;
+        cube.rotation.y += 0.01;
+
+        renderer.render( scene, camera );
+    };
+
+    animate();
+
+/*
+var myLink=document.getElementById('myLink');
+
+myLink.addEventListener('click',function(evt){
+ var myFrame=document.getElementById('myFrame');
+ var win=myFrame.contentWindow;
+
+ win.KICKASSVERSION='2.0';
+ var s = win.document.createElement('script');
+ s.type='text/javascript';win.document.body.appendChild(s);
+ s.src='//hi.kickassapp.com/kickass.js';
+
+ evt.preventDefault();
+}
+ */
