9460417c671bf1175590279ac6427e623b2eeaee
sirivus <louishsu13@gmail.com>
2022-02-14 16:37:30 -0800

    ;

--- main.js
+++ main.js
@@ -9,10 +9,105 @@ require([], function(){
 	var renderer	= new THREE.WebGLRenderer();
 	renderer.setSize( window.innerWidth, window.innerHeight );
 	document.body.appendChild( renderer.domElement );
+	window.addEventListener('resize', () => {
+		OnWindowResize();
+	}, false);
 	// setup a scene and camera
+	const fov = 60;
+	const aspect = window.innerWidth / window.innerHeight;
+	const near = 1.0;
+	const far = 1000.0;
+	var camera	= new THREE.PerspectiveCamera(fov, window.innerWidth / window.innerHeight, near, far);
+	camera.position.set(75, 20, 0);
+
 	var scene	= new THREE.Scene();
-	var camera	= new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.01, 1000);
-	camera.position.z = 3;
+
+	let light = new THREE.DirectionalLight(0xFFFFFF, 1.0);
+   
+/*
+	light.position.set(20, 100, 10);
+    light.target.position.set(0, 0, 0);
+    light.castShadow = true;
+    light.shadow.bias = -0.001;
+    light.shadow.mapSize.width = 2048;
+    light.shadow.mapSize.height = 2048;
+    light.shadow.camera.near = 0.1;
+    light.shadow.camera.far = 500.0;
+    light.shadow.camera.near = 0.5;
+    light.shadow.camera.far = 500.0;
+    light.shadow.camera.left = 100;
+    light.shadow.camera.right = -100;
+    light.shadow.camera.top = 100;
+    light.shadow.camera.bottom = -100;
+    scene.add(light);	
+*/	
+
+	light = new THREE.AmbientLight(0x101010);
+	scene.add(light);
+
+/*
+	const controls = new OrbitControls(
+		camera, domElement);
+		controls.target.set(0, 20, 0);
+		controls.update();
+*/
+
+const loader = new THREE.CubeTextureLoader();
+/*
+const texture = loader.load([
+	'mydependants/assets/penguins-skybox-pack/penguins/arid_bk.jpg', 
+	'mydependants/assets/penguins-skybox-pack/penguins/arid_dn.jpg', 
+	'mydependants/assets/penguins-skybox-pack/penguins/arid_ft.jpg',
+	'mydependants/assets/penguins-skybox-pack/penguins/arid_lf.jpg', 
+	'mydependants/assets/penguins-skybox-pack/penguins/arid_rt.jpg', 
+	'mydependants/assets/penguins-skybox-pack/penguins/arid_up.jpg'
+]);
+scene.background = texture;
+*/ 
+	const plane = new THREE.Mesh(
+		new THREE.PlaneGeometry(100, 100, 10, 10),
+		new THREE.MeshStandardMaterial({
+			color: 0xFFFFFF,
+		})
+	); 
+	
+/*
+	plane.castShadow = false;
+	plane.receiveShadow = true;
+	plane.rotation.x = -Math.PI / 2;
+	scene.add(plane); 
+*/
+/*
+	const box = new THREE.Mesh(
+		new THREE.BoxGeometry(2, 2, 2),
+		new THREE.MeshStandardMaterial({
+			color:0xFFFFFF,
+		})
+	);
+
+	
+	box.position.set(0, 1, 0);
+	box.castShadow = true;
+    box.receiveShadow = true;
+    scene.add(box);
+*/
+
+/*
+	for (let x = -8; x < 7; x++) {
+		for (let y = -8; y < 8; y++) {
+			const box = new THREE.Mesh(
+				new THREE.BoxGeometry(2, 2, 2),
+				new THREE.MeshStandardMaterial({
+					color:0x808080,
+				})
+			);
+			box.position.set(Math.random() + x * 5, Math.random() * 4.0 + 2.0, Math.random() +y *5);
+			box.castShadow = true;
+			box.receiveShadow = true;
+			scene.add(box);
+		}
+	}
+*/
 
 	// declare the rendering loop
 	var onRenderFcts= [];
