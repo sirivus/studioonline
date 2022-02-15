3a598ca22c0fbf2d0e48579f6546e5f65c7043cd
sirivus <louishsu13@gmail.com>
2022-02-15 02:37:09 -0800

    aa

--- gamemod/main.js
+++ gamemod/main.js
@@ -5,24 +5,3 @@ make a game engine
 
 // GAME
 console.log('hi');
-
-const {
-    Worker,
-    isMainThread,
-    setEnvironmentData,
-    getEnvironmentData,
-  } = require('worker_threads');
-  
-  if (isMainThread) {
-    setEnvironmentData('Hello', 'World!');
-    const worker = new Worker(__filename);
-  } else {
-    console.log(getEnvironmentData('Hello'));  // Prints 'World!'.
-  }
-
-  var myWorker = new Worker(__filename);
-
-first.onchange = function() {
-  myWorker.postMessage([first.value,second.value]);
-  console.log('Message posted to worker');
-}
