e0448f40efcb860edee37e2da03aa1f465c0e813
sirivus <louishsu13@gmail.com>
2022-02-15 02:26:25 -0800

    kk

--- gamemod/main1.js
+++ gamemod/main1.js
@@ -1,1 +1,16 @@
 console.log("hi");
+/*
+<script src="node_modules/three.js/node_modules/three/three.js"></script>
+<script src='mydependants/threejs-examples-master/vendor/three.js/build/three.js'></script>
+<script src="mydependants/threejs-examples-master/vendor/require.js/require.js"></script>
+<script src="mydependants/threejs-examples-master/vendor/three.js/examples/js/Detector.js"></script>
+<script src="mydependants/threejs-examples-master/vendor/threex.windowresize.js"></script>
+
+const geometry = new THREE.PlaneGeometry( 1, 1);
+const material = new THREE.MeshBasicMaterial({
+    color: 0xffff00, side: THREE.DoubleSide
+});
+const plane = new THREE.Mesh( geometry, material);
+scene.add(plane);
+
+*/
